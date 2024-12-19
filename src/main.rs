96a478634f871f1d7be470ef752594fa42205e13
Stattek <alienhuntsgames@gmail.com>
2024-12-19 04:46:34 -0600

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+mod sorts;
+
+use sorts::insertion::insertion_sort;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut list = vec![
+        5, 4, 2, 1, 3, 57, 65856, 345, 34534, 321321, 235457, 4, 32, 343, 54334634, 435345353,
+    ];
+    insertion_sort(&mut list, |num1, num2| -> bool { num1 < num2 });
+    println!("{:?}", list);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insertion_sort_test() {
+        // create a list of elements
+        let mut list = vec![4, 5, 2, 1, 3];
+        // sort using a closure to sort elements in ascending order
+        insertion_sort(&mut list, |num1, num2| -> bool { num1 < num2 });
+        assert_eq!(vec![1, 2, 3, 4, 5], list);
+
+        let mut list: Vec<i32> = vec![];
+        insertion_sort(&mut list, |num1, num2| -> bool { num1 < num2 });
+        let test: Vec<i32> = vec![];
+        assert_eq!(test, list);
+
+        // descending sort
+        let mut list: Vec<i32> = vec![4, 5, 2, 1, 3];
+        insertion_sort(&mut list, |num1, num2| -> bool { num1 > num2 });
+        assert_eq!(vec![5, 4, 3, 2, 1], list);
+    }
+}
---
+++ src/sorts/insertion.rs
@@ -0,0 +1,37 @@
+use std::mem::swap;
+
+/// Performs an insertion sort on a list of elements.
+///
+/// # Params
+/// - `list` - The `Vec` to sort.
+/// - `comparison_closure` - The closure to use to sort the array.
+///
+/// # Example
+///
+/// ```rust norun
+///
+/// // create a list of elements
+/// let mut list = vec![4,5,2,1,3];
+/// // sort using a closure to sort elements in ascending order
+/// insertion_sort(&mut list, |num1, num2| -> bool { num1 < num2 });
+/// assert_eq!(vec![1, 2, 3, 4, 5], list);
+///
+/// ```
+pub fn insertion_sort<T, U>(list: &mut Vec<T>, comparison_closure: U)
+where
+    U: Fn(&T, &T) -> bool, // we want a closure to compare the two values and return a bool
+{
+    for i in 1..list.len() {
+        for j in (1..=i).rev() {
+            if comparison_closure(&list[j], &list[j - 1]) {
+                // split the vector to be able to swap behind a mutable reference
+                let (first, second) = list.split_at_mut(j);
+
+                // swap positions j-1 and j
+                swap(&mut first[j - 1], &mut second[0]);
+            } else {
+                break;
+            }
+        }
+    }
+}
