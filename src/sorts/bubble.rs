0b0a1e34a6d5ac91ff8cb2f2ea49b6d842938cbe
Stattek <alienhuntsgames@gmail.com>
2024-12-20 03:27:45 -0600

    Implement bubble sort. - Update tests.

---
+++ src/sorts/bubble.rs
@@ -0,0 +1,47 @@
+use std::mem::swap;
+
+/// Performs a bubble sort on a list of elements.
+/// Implemented using the non-naive approach (checks if the `Vec` is already sorted
+/// and breaks early if so).
+///
+/// # Params
+/// - `list` - The `Vec` to sort.
+/// - `comparison_closure` - The closure to use to sort the array.
+///
+/// # Example
+///
+/// ```rust norun
+///
+/// // create a list of elements
+/// let mut list = vec![4,5,2,1,3];
+/// // sort using a closure to sort elements in ascending order
+/// bubble_sort(&mut list, |first, second| -> bool { first < second });
+/// assert_eq!(vec![1, 2, 3, 4, 5], list);
+///
+/// ```
+pub fn bubble_sort<T, U>(list: &mut Vec<T>, in_order: U)
+where
+    U: Fn(&T, &T) -> bool, // we want a closure to compare the two values and return a bool
+{
+    for i in (1..list.len()).rev() {
+        // assume it is sorted until we find something that is not in order
+        let mut is_sorted = true;
+        for j in 1..=i {
+            if !in_order(&list[j - 1], &list[j]) {
+                // we found something out of order
+                is_sorted = false;
+
+                // split the vector to be able to swap behind a mutable reference
+                let (first, second) = list.split_at_mut(j);
+
+                // swap positions j-1 and j
+                swap(&mut first[j - 1], &mut second[0]);
+            }
+        }
+
+        // break if we find out that the vector is already sorted
+        if is_sorted {
+            break;
+        }
+    }
+}
---
+++ src/tests.rs
@@ -0,0 +1,91 @@
+// ignoring unused import and dead code warnings for tests
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use crate::bubble_sort;
+use crate::insertion_sort;
+use rand::Rng;
+
+#[test]
+fn insertion_sort_test() {
+    let ascending_sort_closure = |num1: &i32, num2: &i32| -> bool { num1 < num2 };
+    let descending_sort_closure = |num1: &i32, num2: &i32| -> bool { num1 > num2 };
+    // create a list of elements
+    let mut list = vec![4, 5, 2, 1, 3];
+    // sort using a closure to sort elements in ascending order
+    insertion_sort(&mut list, ascending_sort_closure);
+    assert_eq!(vec![1, 2, 3, 4, 5], list);
+
+    let mut list: Vec<i32> = vec![];
+    insertion_sort(&mut list, ascending_sort_closure);
+    let test: Vec<i32> = vec![];
+    assert_eq!(test, list);
+
+    let mut list: Vec<i32> = vec![1];
+    insertion_sort(&mut list, ascending_sort_closure);
+    assert_eq!(vec![1], list);
+
+    // descending order sort
+    let mut list: Vec<i32> = vec![4, 5, 2, 1, 3];
+    insertion_sort(&mut list, descending_sort_closure);
+    assert_eq!(vec![5, 4, 3, 2, 1], list);
+
+    let mut list = generate_rand_vec(4000);
+    insertion_sort(&mut list, ascending_sort_closure);
+    is_sorted(&mut list, ascending_sort_closure);
+}
+
+#[test]
+fn bubble_sort_test() {
+    let ascending_sort_closure = |num1: &i32, num2: &i32| -> bool { num1 < num2 };
+    let descending_sort_closure = |num1: &i32, num2: &i32| -> bool { num1 > num2 };
+    // create a list of elements
+    let mut list = vec![4, 5, 2, 1, 3];
+    // sort using a closure to sort elements in ascending order
+    bubble_sort(&mut list, ascending_sort_closure);
+    assert_eq!(vec![1, 2, 3, 4, 5], list);
+
+    let mut list: Vec<i32> = vec![];
+    bubble_sort(&mut list, ascending_sort_closure);
+    let test: Vec<i32> = vec![];
+    assert_eq!(test, list);
+
+    let mut list: Vec<i32> = vec![1];
+    bubble_sort(&mut list, ascending_sort_closure);
+    assert_eq!(vec![1], list);
+
+    // descending order sort
+    let mut list: Vec<i32> = vec![4, 5, 2, 1, 3];
+    bubble_sort(&mut list, descending_sort_closure);
+    assert_eq!(vec![5, 4, 3, 2, 1], list);
+
+    let mut list = generate_rand_vec(4000);
+    bubble_sort(&mut list, ascending_sort_closure);
+    is_sorted(&mut list, ascending_sort_closure);
+}
+
+/// Generates a random vector of `i32`s.
+fn generate_rand_vec(num_elements: u32) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let mut output = vec![];
+    for _ in 0..num_elements {
+        output.push(rng.gen_range(i32::MIN..=i32::MAX));
+    }
+
+    output
+}
+
+/// Checks to see if a vector is sorted.
+fn is_sorted<U, T>(list: &Vec<T>, in_order: U) -> bool
+where
+    U: Fn(&T, &T) -> bool,
+{
+    let mut output = true;
+    for i in 1..list.len() {
+        if !in_order(&list[i - 1], &list[i]) {
+            output = false;
+            break;
+        }
+    }
+
+    output
+}
